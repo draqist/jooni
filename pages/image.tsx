3fb64a7a803200acd9e28c9b3eebc50626f907a9
Abdullah <boomjay008@gmail.com>
2023-03-14 12:21:28 +0100

    setup prettier formating

--- pages/image.tsx
+++ pages/image.tsx
@@ -8,19 +8,28 @@ export default function ImageS() {
 
   useLayoutEffect(() => {
     let rtx = gsap.context(() => {
-      gsap.to(circle.current, {
-        rotation: "360", scale: "0.8"
+      gsap.from(circle.current, {
+        rotation: "360",
+        scale: "0.8",
+        repeat: -1,
+        duration: 3,
+        ease: "none",
+        x: -200,
+        opacity: 0,
       });
-    }, app)
-  
+      gsap.fromTo(".text", { opacity: 0, y: 200 }, { opacity: 1, duration: 3, repeat: -1, y: 0, yoyo: true });
+    }, app);
+
     return () => rtx.revert();
-  }, [])
+  }, []);
 
   return (
     <div ref={app} className="contein">
-      <h1>Image</h1>
-      <div className="circle" ref={circle}>Ref</div>
+      <h1 className="text">Image</h1>
+      <div className="circle" ref={circle}>
+        Ref
+      </div>
       <Image width={1440} alt="preloader" src="/load.jpg" height={500} priority />
     </div>
-  )
+  );
 }
